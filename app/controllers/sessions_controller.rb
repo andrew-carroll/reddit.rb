668bff9c2867fdaeb68af4971e223ec0e317dc90
Andrew Carroll <mrandrewcarroll@gmail.com>
2015-05-03 11:18:13 -0400

    Complete passing code for logging in spec
    Added #create action to SessionsController
    Added login form to sessions/new
    Wrapped forms in sessions/new within section tag

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by_username(params[:username])
+    if @user.authenticate(params[:password])
+      login @user
+      redirect_to session.delete(:return_to) || root_path
+    else
+      flash[:error] = "Your username/password combination is incorrect."
+      redirect_to root_path
+    end
+  end
 end
