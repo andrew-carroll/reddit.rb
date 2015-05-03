756e6fb3e6edb98a97499d7150aaa68ab2e8556d
Andrew Carroll <mrandrewcarroll@gmail.com>
2015-05-03 10:24:39 -0400

    Create #login helper for controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,10 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def login(user)
+    session[:user_id] = user.id
+    user
+  end
+
   helper_method :current_user
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
+      login @user
       redirect_to session.delete(:return_to) || root_path
     else
       flash[:error] = @user.errors.full_messages.to_sentence
@@ -13,7 +13,6 @@ class RegistrationsController < ApplicationController
   private
 
   def user_params
-    params[:password_confirmation] ||= params[:password]
     params.permit(:username, :email, :password, :password_confirmation)
   end
 
