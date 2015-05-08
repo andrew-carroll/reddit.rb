7e4b6e16aa65669d96ddf7a6831943a4095baf57
Andrew Carroll <mrandrewcarroll@gmail.com>
2015-05-08 10:27:08 -0400

    Create failing spec for post creation

---
+++ spec/features/user_creating_post_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+include SessionsHelper
+
+feature 'User creating post' do
+  background do
+    user = create(:user, username: 'programmer') 
+    create(:subreddit, name: 'programming', user: user)
+  end
+  context 'while not logged in' do
+    scenario 'is redirected to the login page' do
+      visit new_subreddit_post_path('programming')
+      expect(page).to have_content 'Sign in'
+    end
+  end
+
+  context 'while logged in' do
+    background do
+      @user = login()
+    end
+    scenario 'succeeds and is redirected to the post' do
+      create_link_post('My first post', 'www.google.com')
+      expect(page).to have_content 'My first post'
+    end
+  end
+end
+
+def create_link_post(title, url)
+  visit new_subreddit_post_path('programming')
+  within 'form#link_post' do
+    fill_in 'title', with: title
+    fill_in 'url', with: url
+    click_button 'submit'
+  end
+end
