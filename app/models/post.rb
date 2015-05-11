b59346073279400134819c491450d58b2d807b40
Andrew Carroll <mrandrewcarroll@gmail.com>
2015-05-11 11:37:00 -0400

    Give each newly created post an upvote by its user

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,13 @@ class Post < ActiveRecord::Base
   has_many :downvotes, class_name: :PostDownvote
   belongs_to :subreddit, required: true
   belongs_to :user, required: true
+
+  after_create :free_upvote
+
+  private
+
+  def free_upvote
+    user.upvote(self)
+  end
+
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe Post, type: :model do
   it { should have_many(:comments) }
   it { should have_many(:upvotes) }
   it { should have_many(:downvotes) }
+  describe 'creation' do
+    it 'gets one free upvote' do
+      post = create(:post)
+      expect(post.upvotes.count).to eq(1)
+    end
+  end
 end
